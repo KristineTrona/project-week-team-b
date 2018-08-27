3ece81b6f671f9875b8889cd79a4c5421aaeaf6e
michaeledander1 <michaeledander1@gmail.com>
2018-08-27 16:46:28 +0200

    images added

--- src/lib/dummyData.js
+++ src/lib/dummyData.js
@@ -2,7 +2,7 @@ const dummyData = [
     {
       id: 1,
       title: "Rattle",
-      imageUrl: "something",
+      imageUrl: "rattle.jpg",
       description: "A nice rattle for your baby to play with.",
       price: 1,
       state: "used",
@@ -11,7 +11,7 @@ const dummyData = [
     {
       id: 2,
       title: "Crib",
-      imageUrl: "something",
+      imageUrl: "crib.jpeg",
       description: "The best sleep your baby will ever have",
       price: 60,
       state: "used",
@@ -20,7 +20,7 @@ const dummyData = [
     {
       id: 3,
       title: "Baby Hat",
-      imageUrl: "something",
+      imageUrl: "babyhat.jpg",
       description: "Cover your baby's head!",
       price: 1.50,
       state: "new",
@@ -29,7 +29,7 @@ const dummyData = [
     {
       id: 4,
       title: "Race Car",
-      imageUrl: "something",
+      imageUrl: "racecar.jpg",
       description: "Super cool, tiny car, that your baby will probably not choke on!",
       price: 7,
       state: "new",
