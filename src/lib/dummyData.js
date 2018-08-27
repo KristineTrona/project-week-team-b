35164df1740a13079dcc9ca6df0109e03d9f66c1
michaeledander1 <michaeledander1@gmail.com>
2018-08-27 16:35:03 +0200

    added dummy data

---
+++ src/lib/dummyData.js
@@ -0,0 +1,38 @@
+const dummyData = [
+    {
+      id: 1,
+      title: "Rattle",
+      imageUrl: "something",
+      description: "A nice rattle for your baby to play with.",
+      price: 1,
+      state: "used",
+      category: "toy"
+    },
+    {
+      id: 2,
+      title: "Crib",
+      imageUrl: "something",
+      description: "The best sleep your baby will ever have",
+      price: 60,
+      state: "used",
+      category: "furniture"
+    },
+    {
+      id: 3,
+      title: "Baby Hat",
+      imageUrl: "something",
+      description: "Cover your baby's head!",
+      price: 1.50,
+      state: "new",
+      category: "clothes"
+    },
+    {
+      id: 4,
+      title: "Race Car",
+      imageUrl: "something",
+      description: "Super cool, tiny car, that your baby will probably not choke on!",
+      price: 7,
+      state: "new",
+      category: "toy"
+    }
+  ]
