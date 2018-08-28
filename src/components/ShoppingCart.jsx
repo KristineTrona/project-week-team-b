086c69a3193b63eab25d641cc810f42017d22d23
Kristine <kristinetrona@gmail.com>
2018-08-28 18:03:55 +0200

    working on the cart item display

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,7 +3,10 @@ import * as React from 'react'
 export default function showImages (props){
     return (
       <div className = "shopping-cart">
-       <div>{props.items.map((item) => item.id)}</div>
+      <div items={props.items.map((item) => (
+       <img src={props.item.imageUrl} alt={props.item.title}/>
+        ))}>
+      </div>
       </div>
     )
   }
