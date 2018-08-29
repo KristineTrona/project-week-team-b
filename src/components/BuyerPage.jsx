125f8066fd90b3400a702fe664c11deeed9d1bbe
vcc1432 <valerie.gersen@gmail.com>
2018-08-29 17:23:34 +0200

    Added some styling to Buyer Page

--- src/components/BuyerPage.jsx
+++ src/components/BuyerPage.jsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
 import Logo_1 from '../lib/images/Logo_1.svg'
-// import noun_wrong_1702071 from "../lib/images/noun_wrong_1702071.svg"
-// import noun_like_1702075 from "../lib/images/noun_like_1702075.svg"
+import noun_wrong_1702071 from "../lib/images/noun_wrong_1702071.svg"
+import noun_like_1702075 from "../lib/images/noun_like_1702075.svg"
 // import shoppingbasket from "../lib/images/shoppingbasket.svg"
 // import pricetag from "../lib/images/pricetag.svg"
 // import beargoodcondition from "../lib/images/beargoodcondition.svg"
 
 export default function showImages (props){
     return (
-      <div>
-      <div className="header">
-      <img src={Logo_1} alt="logo"/>
-      <div className="title">
-      <h1>Kid to Kid</h1>
-      </div>
-      </div>
+      <div className="container">
+        <div className="header">
+          <img src={Logo_1} alt="logo"/>
+          <div className="title">
+            <h1>Kid to Kid</h1>
+          </div>
+        </div>
+
         <div className = "navbar">
           <a>Product</a>
           <a>Gender</a>
@@ -22,33 +23,45 @@ export default function showImages (props){
           <a>Price</a>
           <a>Condition</a>
         </div>
-        
+
+        <div className="content">
+
+          <div className = "product-swipe-info">
+
+          <div className="btn-container">
+            <button className="btn" onClick = {props.handleClickNo}><img src={noun_wrong_1702071}/></button>
+            </div>
+          <div className="product-container"> 
+            <img className="product" src= {props.url} alt="item"/>
+            </div>
+            <div className="btn-container">
+            <button className="btn" onClick = {props.handleClickYes}><img src={noun_like_1702075}/></button> 
+          </div>
+          </div>
+
+            <div className = "description-container">
+              <div className = "description-title">
+                <h3>Title: {props.title}</h3>
+              </div>
+              <div className = "description-description">
+                <p>Description: {props.description}</p>
+              </div>
+          </div>
+          <div className = "description-icon-container">
+            <p><span>Price: {props.price}</span><span>Condition: {props.condition}</span></p>
+            <div className = "bearIcons">{props.condition}</div>
+            <div className= "pricetag">{props.price}</div>
+            <div className= "shopping-basket">{props.count}</div>
+          </div>
         </div>
-        // <div className="content">
-        //   <button id = "left-button" onClick = {props.handleClickNo}><img src={noun_wrong_1702071}/></button>
-        //   <img src= {props.url} alt="item"/>
-        //   <button id = "right-button" onClick = {props.handleClickYes}><img src={noun_like_1702075}/></button> 
-        // </div>
-        // <div className = "product-swipe-info">
-        //   <div className = "description-container">
-        //     <div className = "description-title">
-        //       <h3>Title: {props.title}</h3>
-        //     </div>
-        //     <div className = "description-description">
-        //       <p>Description: {props.description}</p>
-        //     </div>
-        //   </div>
-        //   <div className = "description-icon-container">
-        //     <p><span>Price: {props.price}</span><span>Condition: {props.condition}</span></p>
-        //     <div className = "bearIcons">{props.condition}</div>
-        //     <div className= "pricetag">{props.price}</div>
-        //     <div className= "shopping-basket">{props.count}</div>
-        //   </div>
-        // </div>
-        // <div className = "footer">
-        //   <div className = "footer-contact-buyer">Contact</div>
-        //   <div className = "footer-social-buyer">Social Media</div>
-        // </div>
+        {/* <div className = "footer">
+          <div className = "footer-contact-buyer">Contact</div>
+          <div className = "footer-social-buyer">Social Media</div>
+        </div> */}
+      </div>
+        
+
+   
     )
   }
 
