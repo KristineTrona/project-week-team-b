fbc50080207ae42517dfb4d0c7e19af34d0ab92c
Kristine <kristinetrona@gmail.com>
2018-08-29 09:51:19 +0200

    adding new items fixed again

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,9 +3,9 @@ import * as React from 'react'
 export default function showImages (props){
     return (
       <div className = "shopping-cart">
-      <div items={props.items.map((item) => (
-       <img src={props.item.imageUrl} alt={props.item.title}/>
-        ))}>
+      <div items = {props.items.map(item =>
+        <img src={item.imageUrl} alt={item.title}/>
+      ) }>
       </div>
       <p>test</p>
       </div>
