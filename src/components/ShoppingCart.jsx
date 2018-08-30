5fa947f243c0a60245d15018b4973c768c512e56
michaeledander1 <michaeledander1@gmail.com>
2018-08-30 17:35:27 +0200

    css in progress. photo grid done

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import '../cart.css'
 
 export default class ShoppingCart extends React.PureComponent {
   constructor(props) {
@@ -23,23 +24,28 @@ export default class ShoppingCart extends React.PureComponent {
   render() {
     return (
       <div className = "shopping-cart">
-        <div className = "items">
-          <h3>Shopping Cart</h3>
-          <div className = "shopping-cart-images">
-            {this.props.items.map(item =>
-            <img key={item.id} src={item.imageUrl} alt={item.title} onClick={() => this.props.handleImageClick(item.id)}/>
-            )}
-          </div>
-        </div>
-        <div className = "shopping-cart-details">
-          <h3>Details</h3>
-          <div className="cart-user-details">{this.props.itemTitle}: {this.props.itemDescription} 
-          Price: {this.props.itemPrice}, Condition: {this.props.itemCondition}</div>
-          <div className="cart-item-details">{this.props.userName} Phone: {this.props.userPhone},
-          Email: {this.props.userEmail} City: {this.props.userCity}
-          </div>
-        </div>
-        <p></p>
+        <div className = "cart-info">
+          <div className = "cart-items">
+            <div className= "cart-left-half">
+              <h2>Shopping Cart</h2>
+              <div className = "shopping-cart-images">
+                {this.props.items.map(item =>
+                <img key={item.id} src={item.imageUrl} alt={item.title} onClick={() => this.props.handleImageClick(item.id)}/>
+                )}
+              </div>
+            </div>
+            <div className="cart-right-half">
+              <div className = "shopping-cart-details">
+              <h2>Details</h2>
+                <div className="cart-user-details"><p>{this.props.itemTitle}: {this.props.itemDescription}<br/> 
+                Price: {this.props.itemPrice}<br/> Condition: {this.props.itemCondition}</p></div>
+                <div className="cart-item-details"><h3>Seller</h3><br/><p>Username: {this.props.userName} <br/> Phone: {this.props.userPhone}<br/>
+                Email: {this.props.userEmail}<br/> City: {this.props.userCity}</p>
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>  
       </div>
     )
   }
