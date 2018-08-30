33741e9abc1587d54c4f3d4a91945c16260c0624
Kristine <kristinetrona@gmail.com>
2018-08-30 15:22:15 +0200

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Logo_1 from '../lib/images/Logo_1.svg'
+import { Link } from 'react-router-dom';
 
 export default class Header extends React.PureComponent{
   
@@ -7,7 +8,9 @@ export default class Header extends React.PureComponent{
   render(){
     return (
       <div className="header">
+        <Link to={'/'}>
         <img src={Logo_1} alt="logo"/>
+        </Link>
         <div className="title">
           <h1>
             <span style={{color: "#85BA12"}}>Kid </span>
