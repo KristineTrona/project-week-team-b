eb4362b2d57d0caa8ca4c50217d0a55d2a3ea38d
michaeledander1 <michaeledander1@gmail.com>
2018-08-31 10:07:30 +0200

    minor cart changes to css and js

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,32 +3,18 @@ import '../cart.css'
 
 export default class ShoppingCart extends React.PureComponent {
   constructor(props) {
-    super(props)    
-    this.state = {
-      
-    }
-  }
-
-
-  handleChange = (event) => {
-    const value = event.target.value
-    const id = event.target.id
-
-    this.setState(
-      {
-      [id]: value
-      }
-    )
+    super(props);
   }
   
   render() {
+    // if (this.state.itemOwner.userId) {
     return (
       <div className = "shopping-cart">
         <div className = "cart-info">
           <div className = "cart-items">
             <div className= "cart-left-half">
               <h2>Shopping Cart</h2>
-              <div className = "shopping-cart-images">
+              <div className = "shopping-cart-images" >
                 {this.props.items.map(item =>
                 <img key={item.id} src={item.imageUrl} alt={item.title} onClick={() => this.props.handleImageClick(item.id)}/>
                 )}
@@ -37,7 +23,8 @@ export default class ShoppingCart extends React.PureComponent {
             <div className="cart-right-half">
               <div className = "shopping-cart-details">
               <h2>Details</h2>
-                <div className="cart-user-details"><p>{this.props.itemTitle}: {this.props.itemDescription}<br/> 
+              <div className="cart-user-details"><p><img className="selected-cart-item" src={this.props.currentItem} alt={this.props.itemTitle}/>
+                {this.props.itemTitle}: {this.props.itemDescription}<br/> 
                 Price: {this.props.itemPrice}<br/> Condition: {this.props.itemCondition}</p></div>
                 <div className="cart-item-details"><h3>Seller</h3><br/><p>Username: {this.props.userName} <br/> Phone: {this.props.userPhone}<br/>
                 Email: {this.props.userEmail}<br/> City: {this.props.userCity}</p>
@@ -50,3 +37,28 @@ export default class ShoppingCart extends React.PureComponent {
     )
   }
 }
+//   } else {
+//     return (
+//       <div className = "shopping-cart">
+//         <div className = "cart-info">
+//           <div className = "cart-items">
+//             <div className= "cart-left-half">
+//               <h2>Shopping Cart</h2>
+//               <div className = "shopping-cart-images">
+//                 {this.props.items.map(item =>
+//                 <img key={item.id} src={item.imageUrl} alt={item.title} onClick={() => this.props.handleImageClick(item.id)}/>
+//                 )}
+//               </div>
+//             </div>
+//             <div className="cart-right-half">
+//               <div className = "shopping-cart-details">
+//               <h2>Select a product to see more details and seller contact information</h2>
+//               </div>
+//             </div>
+//           </div>  
+//         </div>  
+//       </div>      
+//     )
+
+//   }
+// }}
